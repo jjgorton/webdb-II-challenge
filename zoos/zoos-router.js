d881a39076e643e332108f8fab9c81851ee63b36
jjgorton <jjgorton@gmail.com>
2019-04-30 17:53:12 -0400

    MVP Complete

--- zoos/zoos-router.js
+++ zoos/zoos-router.js
@@ -11,4 +11,87 @@ const knexConfig = {
 
 const db = knex(knexConfig);
 
+router.get('/', (req, res) => {
+	db('zoos')
+		.then((zoos) => {
+			res.status(200).json(zoos);
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
+
+router.get('/:id', (req, res) => {
+	db('zoos')
+		.where({ id: req.params.id })
+		.first()
+		.then((zoos) => {
+			if (zoos) {
+				res.status(200).json(zoos);
+			} else {
+				res.status(404).json({ message: 'Mising Monkey, No Zoos found.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
+
+router.post('/', (req, res) => {
+	if (!req.body.name) {
+		res.status(400).json({ message: 'Provide a name, you silly sloth!' });
+	} else {
+		db('zoos')
+			.insert(req.body, 'id')
+			.then((ids) => {
+				db('zoos').where({ id: ids[0] }).first().then((zoos) => {
+					res.status(200).json(zoos);
+				});
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
+});
+
+router.delete('/:id', (req, res) => {
+	db('zoos')
+		.where({ id: req.params.id })
+		.del()
+		.then((count) => {
+			if (count > 0) {
+				res.status(200).json({
+					message : `${count} ${count > 1 ? 'records' : 'record'} deleted.`
+				});
+			} else {
+				res.status(404).json({ message: 'Mising Monkey, No Zoos found.' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
+
+router.put('/:id', (req, res) => {
+	if (!req.body.name) {
+		res.status(400).json({ message: 'Provide a name, you silly sloth!' });
+	} else {
+		db('zoos')
+			.where({ id: req.params.id })
+			.update(req.body)
+			.then((count) => {
+				if (count > 0) {
+					res.status(200).json({
+						message : `${count} ${count > 1 ? 'records' : 'record'} updated`
+					});
+				} else {
+					res.status(404).json({ message: 'Mising Monkey, No Zoos found.' });
+				}
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
+});
+
 module.exports = router;
